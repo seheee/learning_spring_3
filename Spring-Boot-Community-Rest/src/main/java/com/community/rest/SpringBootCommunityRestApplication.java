a58dc38c8fe18c232e5ba678c257ea1fe2a9a701
seheee <4554010@naver.com>
2021-02-04 23:03:57 +0900

    init

---
+++ Spring-Boot-Community-Rest/src/main/java/com/community/rest/SpringBootCommunityRestApplication.java
@@ -0,0 +1,13 @@
+package com.community.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCommunityRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCommunityRestApplication.class, args);
+	}
+
+}
---
+++ Spring-Boot-Community-Web/src/main/java/com/web/domain/enums/SocialType.java
@@ -0,0 +1,2 @@
+package com.web.domain.enums;public enum SocialType {
+}
